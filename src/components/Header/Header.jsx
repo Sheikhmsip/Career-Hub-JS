7afc7c34305255befe885481ef2462b2db452b00
Sheikhmsip <sheikhmsip@gmail.com>
2023-04-10 00:49:14 +0600

    added navbar with mobile responsive

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import NavBar from '../NavBar/NavBar';
+
+const Header = () => {
+    return (
+        
+            <NavBar></NavBar>
+        
+
+
+     
+    );
+};
+
+export default Header;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,30 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import ReactDOM from "react-dom/client";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Main from './components/Layout/Main';
+import Header from './components/Header/Header';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Main></Main>,
+    children:[
+      {
+        path: 'header',
+        element: <Header></Header>
+      }
+    ]
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <RouterProvider router={router}></RouterProvider>
   </React.StrictMode>,
 )

5ccef293829242cb13b96e6a6d4be5bc3d760141
Sheikhmsip <sheikhmsip@gmail.com>
2023-04-11 17:37:35 +0600

    added job details dynamically

--- src/components/JobDetails/JobDetails.jsx
+++ src/components/JobDetails/JobDetails.jsx
@@ -3,22 +3,48 @@ import { useLoaderData, useParams } from 'react-router-dom';
 
 const JobDetails = () => {
     const data = useLoaderData();
-    let {id} = useParams();
+    let { id } = useParams();
     // console.log(jobs)
     const jobs = data.find(job => job.id === id)
-        console.log(jobs.id)
-        const {description , name, salary } = jobs;
-   
+    console.log(jobs.id)
+    const { descriptions, responsibility, name, salary, phone, email, address, requirements, experiences } = jobs;
+
     // if(data) {
     //         data.find(job => job.id === id)
     //         console.log(jobs.id)
     //     }
-    
+
 
     return (
-        <div>
-            <h1>{salary}</h1>
-        </div>
+        <>
+            <h1 className='text-center font-bold text-3xl'>Job Details</h1>
+            <div className='grid md:grid-cols-3 mt-20 w-[96%] mx-auto gap-2'>
+                <div className=' md:col-span-2'>
+                    <p className='pb-3'><span className='font-bold'>Descriptions: </span>{descriptions}</p>
+                    <p className='pb-3'><span className='font-bold'>Responsibility: </span> {responsibility}</p>
+                    <p className='pb-3'><span className='font-bold'>Requirements:</span> {requirements}</p>
+                    <p><span className='font-bold'>Experiences</span> {experiences}</p>
+                </div>
+                <div>
+                    <div className='px-3 bg-[#cdd1eb] rounded-md py-5'>
+                        <h2 className='text-xl font-bold pb-3 '>Job Details</h2>
+                        <hr className='px-2 py-2' />
+                        <p className=' pl-1'><span className='font-bold'>Salary:</span> <span>{salary}</span></p>
+                        <p className='pl-1'><span className='font-bold'>Job Title:</span> <span>{name}</span></p>
+                        <p className=' text-xl font-bold pt-4 pb-2'>Contact Information</p>
+                        <hr className='pb-2' />
+                        <p className='pl-1'><span className='font-bold'>Phone:</span> <span>{phone}</span></p>
+                        <p className='py-1 pl-1'><span className='font-bold'>Email:</span> <span>{email}</span></p>
+                        <p className='pl-1'><span className='font-bold'>Address:</span> <span>{address}</span></p>
+                    </div>
+                    <button className='w-full btn bg-gradient-to-r from-purple-500 to-purple-400 ... mt-5'>Apply Now</button>
+
+
+                </div>
+
+
+            </div>
+        </>
     );
 };
 

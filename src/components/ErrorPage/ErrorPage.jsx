3055f033f4b72f4eba0b8aa554f043e83c71bab8
Sheikhmsip <sheikhmsip@gmail.com>
2023-04-12 13:19:40 +0600

    modify error page and redme file

--- src/components/ErrorPage/ErrorPage.jsx
+++ src/components/ErrorPage/ErrorPage.jsx
@@ -3,12 +3,12 @@ import { Link, useRouteError } from 'react-router-dom'
 const ErrorPage = () => {
     const { error, status } = useRouteError()
     return (
-        <section className='flex items-center h-screen p-16 bg-gray-100 text-gray-900'>
+        <section className='flex items-center h-screen p-16 bg-purple-100 text-purple-900'>
       <div className='container flex flex-col items-center justify-center px-5 mx-auto my-8'>
         <svg
           xmlns='http://www.w3.org/2000/svg'
           viewBox='0 0 512 512'
-          className='w-40 h-40 text-gray-600'
+          className='w-40 h-40 text-purple-600'
         >
           <path
             fill='currentColor'
@@ -31,17 +31,17 @@ const ErrorPage = () => {
           ></polygon>
         </svg>
         <div className='max-w-md text-center'>
-          <h2 className='mb-8 font-extrabold text-9xl text-gray-600'>
-            <span className='sr-only'>Error</span> {status || 404}
+          <h2 className='mb-8 font-extrabold text-9xl text-purple-600'>
+            <span className='sr-only'>Error</span> {status || 404 }
           </h2>
           <p className='text-2xl font-semibold md:text-3xl mb-8'>
-            {error?.message}
+            you are out of the world
           </p>
           <Link
             to='/'
-            className='px-8 py-3 font-semibold rounded bg-purple-500 text-gray-900'
+            className='px-8 py-3 font-semibold rounded bg-purple-500 text-purple-900'
           >
-            Back to Home Page
+            Back to World
           </Link>
         </div>
       </div>
